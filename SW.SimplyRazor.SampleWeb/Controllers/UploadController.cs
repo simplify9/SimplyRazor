f7c0e6495a51d777371da7e0e08fb3bb17f0de77
Samer Awajan <samer@simplify9.com>
2019-10-18 03:29:08 +0300

    upload enhancements

---
+++ SW.SimplyRazor.SampleWeb/Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SW.SimplyRazor.SampleWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload([FromForm] UploadReuqest ur)
+        {
+            //ur.File.
+            return new OkResult();
+        }
+
+
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+        public class UploadReuqest
+        {
+            public string Id { get; set; }
+            public IFormFile File { get; set; }
+        }
+    }
+}
--- SW.SimplyRazor/Components/SimplyUpload/SimplyUploadInterop.js
+++ SW.SimplyRazor/Components/SimplyUpload/SimplyUploadInterop.js
@@ -2,17 +2,20 @@
     window.simplyUploadInterop = {
 
         saveFile: function (element) {
-            //let user = { name: 'john', age: 34 };
+            //var id = { name: 'john', age: 34 };
             var formData = new FormData();
             var file = element.files[0];
             //Promise.all(files.map(f => fetch('/upload', { method: "POST", body: formData })));
-            formData.append("file", file);
+            formData.append("File", file);
+            formData.append("Id", "123456");
+
+
             //formData.append("user", JSON.stringify(user));
 
             //try {
 
-            return fetch('/upload', {method: "POST", body: formData })
-                .then(response => console.log('HTTP response code:', response.status));
+            return fetch('api/upload', {method: "POST", body: formData })
+                .then(response => console.log(response.body));
             //}
             //catch (e) {
             //    console.log('Huston we have problem...:', e);
