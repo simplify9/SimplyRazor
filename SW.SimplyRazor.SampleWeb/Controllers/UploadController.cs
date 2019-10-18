53bf51d70a08aa9d00d4c5d76b4ca364b61f91c3
Samer Awajan <samer@simplify9.com>
2019-10-18 17:09:02 +0300

    more upload features

--- SW.SimplyRazor.SampleWeb/Controllers/UploadController.cs
+++ SW.SimplyRazor.SampleWeb/Controllers/UploadController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SW.SimplyRazor.SampleWeb.Controllers
 {
@@ -22,24 +22,32 @@ namespace SW.SimplyRazor.SampleWeb.Controllers
 
         // POST api/<controller>
         [HttpPost, DisableRequestSizeLimit]
-        public IActionResult Upload([FromForm] UploadReuqest ur)
+        public async Task<IActionResult> Upload([FromForm] UploadReuqest ur)
         {
-            //ur.File.
-            return new OkResult();
+            Directory.CreateDirectory("./uploads");
+
+            var fileId = Guid.NewGuid().ToString("N");
+            var filePath = $"./uploads/{fileId}";
+
+            await Task.Delay(TimeSpan.FromSeconds(2)); 
+
+            using (var fs = System.IO.File.Create(filePath))
+            {
+                await ur.File.CopyToAsync(fs);
+            };
+            return new BadRequestResult(); 
+            //return new OkObjectResult(fileId);
         }
 
-
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
 
         public class UploadReuqest
         {
-            public string Id { get; set; }
+            //public string Id { get; set; }
             public IFormFile File { get; set; }
         }
     }
--- SW.SimplyRazor/Components/SimplyUpload/SimplyUploadInterop.js
+++ SW.SimplyRazor/Components/SimplyUpload/SimplyUploadInterop.js
@@ -7,15 +7,16 @@
             var file = element.files[0];
             //Promise.all(files.map(f => fetch('/upload', { method: "POST", body: formData })));
             formData.append("File", file);
-            formData.append("Id", "123456");
+            //formData.append("Id", "123456");
 
 
             //formData.append("user", JSON.stringify(user));
 
             //try {
 
-            return fetch('api/upload', {method: "POST", body: formData })
-                .then(response => console.log(response.body));
+            return fetch('api/upload', { method: "POST", body: formData })
+                .then(response => response.text());
+                //.then((body) => console.log(body));
             //}
             //catch (e) {
             //    console.log('Huston we have problem...:', e);
