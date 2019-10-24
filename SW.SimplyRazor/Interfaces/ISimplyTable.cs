35202c9caac81a776fb73235d5e27f14b8d4b770
Samer Awajan <samer@simplify9.com>
2019-10-24 18:57:44 +0400

    ISimplyTable

---
+++ SW.SimplyRazor/Interfaces/ISimplyTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.SimplyRazor
+{
+    public interface ISimplyTable
+    {
+        Task AddModel();
+        Task EditModel(int index);
+        Task DeleteModel(int index);
+            
+    }
+}
