83b386cf2d232afb5024c4596487c8262ffbdba9
Samer Awajan <samer@simplify9.com>
2020-01-13 11:10:44 +0200

    ..

---
+++ SW.SimplyRazor/Components/SimplyTable/ITableAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.SimplyRazor
+{
+    public interface ITableAction
+    {
+        Task Add();
+        Task RemoveAll();
+    }
+}
