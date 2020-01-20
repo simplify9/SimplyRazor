e526e4ae0f7a0a5f0b00ffed9a7fa6f0fe31a1a5
Samer Awajan <samer@simplify9.com>
2020-01-20 13:39:17 +0200

    SimplyTabs

---
+++ SW.SimplyRazor/Components/SimplyTabs/ITab.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.SimplyRazor
+{
+    public interface ITab
+    {
+        RenderFragment ChildContent { get; }
+    }
+}
