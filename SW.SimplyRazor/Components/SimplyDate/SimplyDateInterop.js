e90a1d3462c6b8c729583167f5b74f54a3e8b0b2
Samer Awajan <samer@simplify9.com>
2020-02-07 16:26:08 +0200

    SimplyDate

---
+++ SW.SimplyRazor/Components/SimplyDate/SimplyDateInterop.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    window.simplyDateInterop = {
+
+        init: function (element, dotNetObjRef, id, defaultDate, mode) {
+
+            window[id] = flatpickr(element, {
+                defaultDate: defaultDate,
+                mode: mode,
+                allowInput: ((mode == 'single') ? true : false),
+                onValueUpdate: function (selectedDates, dateStr, instance) {
+                    dotNetObjRef.invokeMethodAsync('OnValueUpdate', selectedDates);
+                },
+                onChange: function (selectedDates, dateStr, instance) {
+                    dotNetObjRef.invokeMethodAsync('OnChange', selectedDates);
+                },
+
+            });
+
+        },
+
+        destroy: function (id) {
+
+            window[id].destroy();
+        },
+
+        //disable: function (id) {
+
+        //    window[id].disable();
+        //},
+
+        //enable: function (id) {
+
+        //    window[id].enable();
+        //},
+
+    };
+
+})();
--- SW.SimplyRazor/gulpfile.js
+++ SW.SimplyRazor/gulpfile.js
@@ -3,7 +3,9 @@
 /// <reference path="components/simplychoices/simplychoicesinterop.js" />
 /// <reference path="components/simplyupload/simplyuploadinterop.js" />
 /// <reference path="components/simplyalert/simplyalertinterop.js" />
+/// <reference path="node_modules/flatpickr/dist/flatpickr.min.js" />
 
+/// <reference path="components/simplydate/simplydateinterop.js" />
 /// <reference path="node_modules/choices.js/public/assets/scripts/choices.js" />
 /*
 Gulp file....
@@ -24,9 +26,10 @@ gulp.task('js', function () {
         //'node_modules/@yaireo/tagify/dist/tagify.js',
         'node_modules/bs-custom-file-input/dist/bs-custom-file-input.js',
         'node_modules/choices.js/public/assets/scripts/choices.js',
+        'node_modules/flatpickr/dist/flatpickr.min.js',
         //'node_modules/bootstrap.native/dist/bootstrap-native-v4.js',
         //'node_modules/tabulator-tables/dist/js/tabulator.min.js',
-
+        'components/simplydate/simplydateinterop.js',
         'components/simplyeditor/simplyeditorinterop.js',
         //'components/simplytags/simplytagsinterop.js',
         'components/simplychoices/simplychoicesinterop.js',
@@ -48,6 +51,7 @@ gulp.task('css', function () {
         'node_modules/choices.js/public/assets/styles/choices.css',
         'components/simplychoices/simplychoices.css',
         'node_modules/quill/dist/quill.snow.css',
+        'node_modules/flatpickr/dist/flatpickr.min.css',
         //'node_modules/tabulator-tables/dist/css/bootstrap/tabulator_bootstrap.min.css',
 
     ])
