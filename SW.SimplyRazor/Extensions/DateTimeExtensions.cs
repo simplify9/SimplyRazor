d460f8bb2a76da98927bd7974d7d33a192473a48
Samer Awajan <samer@simplify9.com>
2020-09-29 11:43:25 +0300

    fix date local/utc issue

---
+++ SW.SimplyRazor/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.SimplyRazor
+{
+    internal static class DateTimeExtensions
+    {
+        public static DateTime ChangeKind(this DateTime dateTime, DateTimeKind kind = DateTimeKind.Unspecified)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second, kind);
+        }
+    }
+}
